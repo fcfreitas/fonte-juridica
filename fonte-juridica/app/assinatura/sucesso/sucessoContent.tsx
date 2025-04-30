0136c8462617c68ff463ed31c5a464d00184bb27
Fabio da Costa Freitas <fabio.freitas@nubank.com.br>
2025-04-30 21:06:31 +0000

    modified:   fonte-juridica/app/assinatura/sucesso/sucessoContent.tsx

--- fonte-juridica/app/assinatura/sucesso/sucessoContent.tsx
+++ fonte-juridica/app/assinatura/sucesso/sucessoContent.tsx
@@ -14,14 +14,15 @@ export default function SucessoContent() {
   }, [sessionId]);
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen p-6">
+    <div className="flex flex-col items-center justify-center min-h-[70vh] p-6">
       <h1 className="text-3xl font-bold text-green-600 mb-4">Assinatura Confirmada!</h1>
-      <p className="text-lg text-center mb-6">Obrigado por assinar o Fonte Jurídica.</p>
+      <p className="text-lg text-center font-semibold w-3/4">Obrigado por assinar o Fonte Jurídica. </p>
+      <p className="text-lg text-center mb-6 w-3/4">Agora você tem acesso direto aos julgados mais relevantes do STF, organizados e comentados para facilitar seus estudos.</p>
       <a
         href="/home"
         className="bg-blue-600 hover:bg-blue-700 text-white font-semibold py-2 px-6 rounded-xl transition"
       >
-        Voltar para Home
+        Ampliar meus conhecimentos jurídicos
       </a>
     </div>
   );
